6418fb6ab7c4689748ac1eb117af5ee4e68a24de
Esmith22306 <Ethankip@gmail.com>
2025-05-23 14:40:34 -0600

    commit

--- obj/Debug/net8.0/MiniSudoku.AssemblyInfo.cs
+++ obj/Debug/net8.0/MiniSudoku.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiniSudoku")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2e1e9694b27743748ecc738d44a69f6f502ead1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiniSudoku")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiniSudoku")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
